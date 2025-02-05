5934a89e847cb0897aa872db0f111cbe6d98271f
igleite <51419192+igleite@users.noreply.github.com>
2025-02-05 17:23:21 -0300

    fix: corrigir formatação dos segundos no método converterSegundosParaHHMMSS

--- src/time/time-utils.ts
+++ src/time/time-utils.ts
@@ -46,7 +46,7 @@ export class TimeUtils {
 
       const horas: number = Math.floor(segundos / 3600);
       const minutos: number = Math.floor((segundos % 3600) / 60);
-      const segundosRestantes: number = segundos % 60;
+      const segundosRestantes: number = Math.floor(segundos % 60);
 
       return `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}:${segundosRestantes.toString().padStart(2, '0')}`;
     } catch (error) {
